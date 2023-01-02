c5aa86be1a793cc05a8de48741be06c7fcece22c
Dmytro <dm.kalitventsev@gmail.com>
2023-01-02 19:41:01 +0200

    changed the structure of work and add js + sticky menu + svg

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+	const targetItem = e.target;
+	if (targetItem.closest('.menu-button')) {
+		document.documentElement.classList.toggle('menu-open');
+	}
+}
